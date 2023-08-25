ac068ff8ec3d8dc75cd71e4ffff7bc7d8c98926d
Lyahorskiy <cibulyka@gmail.com>
2023-08-25 20:47:35 +0300

    Попробовал редакс. Создал каунтер

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { createContext, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './redux/slices/filterSlice'
 
 import './scss/app.scss'
 import Header from './components/Header'
@@ -10,10 +12,27 @@ import Cart from './pages/Cart'
 export const SearchContext = createContext('')
 function App() {
   const [searchValue, setSearchValue] = useState('')
+  const count = useSelector((state) => state.counter.count)
+  const dispatch = useDispatch()
+
   console.log(searchValue, 'Input chenged')
   return (
     <div className="wrapper">
-      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <span>{count}</span>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
+
+      {/* <SearchContext.Provider value={{ searchValue, setSearchValue }}>
         <Header />
         <div className="content">
           <div className="container">
@@ -24,7 +43,7 @@ function App() {
             </Routes>
           </div>
         </div>
-      </SearchContext.Provider>
+      </SearchContext.Provider> */}
     </div>
   )
 }
